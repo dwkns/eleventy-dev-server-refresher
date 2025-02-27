8c795251b80dc9209b6aff2f3645c6cb74ec1e56
dwkns <dwkns@me.com>
2025-02-27 17:55:41 +0000

    Added template & refresh code.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,23 @@
 
 module.exports = (eleventyConfig) => {
+
+
+  eleventyConfig.setServerOptions({
+    onRequest: {
+      "/reload/:template": function ({ url, pattern, patternGroups }) {
+        const template = atob(patternGroups.template) // decode the b64 encocded string
+        console.log("Someone clicked the `Tell the Dev Server to do stuff` button")
+        console.log(`template: ${template} was passed back from the served page`) // tells us which template needs to change
+       
+        // code to reprocess data cascade
+        // code to rebuild template
+       
+        return "Nothing to see here" 
+      }
+    }
+  });
+
+
   return {
     dir: {
       input: "src",
---
+++ src/_data/site.js
@@ -0,0 +1,5 @@
+ // simulate a API call. data is only updated when the data cascade is processed.
+ 
+module.exports ={
+  date: Date.now()
+}
